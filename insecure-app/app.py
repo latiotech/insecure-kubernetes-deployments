6a8380643291e17a9e79bb3356ac2071d1e40102
James Berthoty <confusedcrib@gmail.com>
2023-11-20 22:35:57 -0500

    testing ox

--- insecure-app/app.py
+++ insecure-app/app.py
@@ -34,7 +34,7 @@ def index():
         </form>
         <pre>{{output}}</pre>
     """, output=output)
-
+print("hi")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
