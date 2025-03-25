d5aca0b40047f316299b01dfb68af390dbe05709
James Berthoty <confusedcrib@gmail.com>
2025-03-24 22:12:33 -0400

    janky tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,32 +60,32 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
+    main = {
+      name = "main-node-group"
       instance_types = ["t3.xlarge"]
-
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
+      max_size     = 6
+      desired_size = 3
 
-    two = {
-      name = "node-group-2"
+      # Add security group to the node group
+      vpc_security_group_ids = [aws_security_group.worker_group_mgmt.id]
 
-      instance_types = ["t3.xlarge"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      # Add these configurations for faster termination
+      force_update_version = true
+      force_delete = true
+      
+      # Reduce the time pods have to gracefully terminate
+      timeouts = {
+        create = "30m"
+        update = "30m"
+        delete = "15m"
+      }
     }
   }
 }
-
 
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
@@ -112,4 +112,133 @@ resource "aws_eks_addon" "ebs-csi" {
     "eks_addon" = "ebs-csi"
     "terraform" = "true"
   }
+}
+
+# Create security group for worker nodes
+resource "aws_security_group" "worker_group_mgmt" {
+  name_prefix = "worker_group_mgmt"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "1.2.3.4/32",  # Replace with your IP address
+      module.vpc.vpc_cidr_block,
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "worker-group-mgmt"
+  }
+}
+
+# Create IAM role for AWS Load Balancer Controller
+module "lb_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "aws-load-balancer-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+# Install AWS Load Balancer Controller
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.lb_role.iam_role_arn
+  }
+
+  depends_on = [module.eks]
+}
+
+# Install ingress-nginx using Helm
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  values = [
+    <<-EOT
+    controller:
+      ingressClassResource:
+        name: nginx
+        default: true
+      service:
+        annotations:
+          service.beta.kubernetes.io/aws-load-balancer-type: "nlb"
+          service.beta.kubernetes.io/aws-load-balancer-backend-protocol: "tcp"
+          service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: "true"
+          service.beta.kubernetes.io/aws-load-balancer-scheme: "internet-facing"
+          service.beta.kubernetes.io/aws-load-balancer-nlb-target-type: "ip"
+          service.beta.kubernetes.io/aws-load-balancer-additional-resource-tags: "ingress-nginx=true,kubernetes.io/cluster/${module.eks.cluster_name}=owned"
+        type: LoadBalancer
+        externalTrafficPolicy: Local
+        labels:
+          ingress-nginx: "true"
+      config:
+        use-forwarded-headers: "true"
+        use-proxy-protocol: "false"
+    EOT
+  ]
+
+  depends_on = [module.eks, helm_release.aws_load_balancer_controller]
+}
+
+# Add a time_sleep to wait for LB creation
+resource "time_sleep" "wait_for_lb" {
+  create_duration = "30s"
+  triggers = {
+    # This will change whenever either helm release changes
+    helm_id = "${helm_release.ingress_nginx.id}${helm_release.aws_load_balancer_controller.id}"
+  }
+  depends_on = [
+    helm_release.ingress_nginx,
+    helm_release.aws_load_balancer_controller
+  ]
+}
+
+# Data source to get the NLB created by ingress-nginx
+data "aws_lb" "ingress" {
+  tags = {
+    "ingress-nginx"                            = "true"
+    "kubernetes.io/cluster/${module.eks.cluster_name}" = "owned"
+  }
+
+  depends_on = [
+    time_sleep.wait_for_lb,
+    helm_release.aws_load_balancer_controller
+  ]
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.34.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.24.0"
     }
 
     random = {
@@ -19,7 +27,34 @@ terraform {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.2"
     }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13.0"
+    }
   }
 
-  required_version = "~> 1.3"
+  required_version = ">= 1.2.0"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
 }
